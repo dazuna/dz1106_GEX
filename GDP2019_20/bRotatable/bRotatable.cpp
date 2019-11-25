44bfa5c81eb949843c834ec13ba6f9fab06a795a
Dzuniga <dave.zuna@gmail.com>
2019-11-25 14:42:24 -0500

    get original at

---
+++ GDP2019_20/bRotatable/bRotatable.cpp
@@ -0,0 +1,57 @@
+#include "bRotatable.hpp"
+
+// always the same since it's the original loading state
+glm::vec3 bRotatable::ogAT = glm::vec3(0,0,1);
+glm::vec3 bRotatable::ogUP = glm::vec3(0,1,0);
+
+// Magic Methods
+glm::vec3 bRotatable::getCurrentAT()
+{
+	return (this->qOrientation * this->ogAT);
+}
+glm::vec3 bRotatable::getCurrentUP()
+{
+	return (this->qOrientation * this->ogUP);
+}
+// Overwrite the orientation
+void bRotatable::setOrientation(glm::vec3 EulerAngleDegreesXYZ)
+{
+	glm::vec3 EulerAngleRadians;
+	EulerAngleRadians = glm::radians(EulerAngleDegreesXYZ);
+	this->qOrientation = glm::quat(EulerAngleRadians);
+}
+void bRotatable::setOrientation(glm::quat qAngle)
+{
+	this->qOrientation = qAngle;
+}
+// Updates the existing angle
+void bRotatable::updateOrientation(glm::vec3 EulerAngleDegreesXYZ)
+{
+	glm::vec3 EulerAngleRadians;
+	EulerAngleRadians = glm::radians(EulerAngleDegreesXYZ);
+	glm::quat angleChange = glm::quat(EulerAngleRadians);
+	this->qOrientation *= angleChange;
+}
+void bRotatable::updateOrientation(glm::quat qAngle)
+{
+	this->qOrientation *= qAngle;
+}
+
+// For saving it in the JSON
+glm::vec3 bRotatable::getEulerAngle(void)
+{
+	glm::vec3 EulerAngle = glm::eulerAngles(this->qOrientation);
+	EulerAngle = glm::degrees(EulerAngle);
+	return EulerAngle;
+}
+
+void bRotatable::setAT(glm::vec3 direction)
+{
+	this->qOrientation = RotationBetweenVectors(this->ogAT, direction);
+}
+
+glm::vec3 bRotatable::getOG_AT()
+{
+	return this->ogAT;
+}
+
---
+++ GDP2019_20/bRotatable/bRotatable.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <string>
+#include <iostream>
+#include "../quaternion_utils/quaternion_utils.hpp"
+
+class bRotatable
+{
+public:
+    glm::quat getQOrientation() {return this->qOrientation;}
+    glm::vec3 getCurrentAT();
+    glm::vec3 getCurrentUP();
+    // Overwrite the orientation
+	void setOrientation(glm::vec3 EulerAngleDegreesXYZ);
+	void setOrientation(glm::quat qAngle);
+	// Updates the existing angle
+	void updateOrientation(glm::vec3 EulerAngleDegreesXYZ);
+	void updateOrientation(glm::quat qAngle);
+	glm::vec3 getEulerAngle(void);
+	// setAT ... for light manipulation
+	void setAT(glm::vec3 direction);
+	glm::vec3 getOG_AT();
+private:
+    glm::quat qOrientation;
+    static glm::vec3 ogAT;
+    static glm::vec3 ogUP;
+};
